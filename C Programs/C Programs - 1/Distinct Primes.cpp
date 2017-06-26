b0bafa84ef4b2e25b1c5a632202be83e7300d932
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-06-26 17:45:58 +0530

    Add files via upload

---
+++ C Programs/C Programs - 1/Distinct Primes.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+vector <int> at_least_three_prime_factors;
+
+void precompute_prime_factors()
+{
+    vector <int> no_of_prime_factors(1e5, 0);
+
+    for(int i = 2; i <= 1e5; i++)
+        if(no_of_prime_factors[i] == 0)
+            for(int multiple = i; multiple <= 1e5; multiple += i)
+            {
+                no_of_prime_factors[multiple]++;
+            }
+
+    for(int i = 2; i <= 1e5 && at_least_three_prime_factors.size() <= 1000; i++)
+        if(no_of_prime_factors[i] >= 3)
+            at_least_three_prime_factors.push_back(i);
+
+}
+
+int main()
+{
+    int no_of_test_cases;
+    scanf("%d", &no_of_test_cases);
+
+    precompute_prime_factors();
+
+    while(no_of_test_cases--)
+    {
+        int query;
+        scanf("%d", &query);
+
+        printf("%d\n",at_least_three_prime_factors[query - 1]);
+    }
+
+    return 0;
+}
